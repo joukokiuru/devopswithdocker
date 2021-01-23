6d8b8d985ccab0f20651f42c504b9e960c105896
Jouko Kiuru <58884648+joukokiuru@users.noreply.github.com>
2021-01-23 11:22:46 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create copy of the source repository
+repo="https://github.com/joukokiuru/original-repo.git"
+git clone $repo
+
+# Build image
+cd original-repo .
+docker build -t joukokiuru/original-repo .
+
+# Push copy to the destination in Docker Hub
+docker login
+docker push "joukokiuru/original-repo"
+
